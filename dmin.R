55b1204c06c85d8f8dd5fff03826ca33954ae918
Argyris Zardilis <azardilis@gmail.com>
2014-03-03 22:53:59 +0000

    added functions for exploration of parameters, an objective function for optimisation using R's optim and an mcmc for bayesian parameter estimation

--- dmin.R
+++ dmin.R
@@ -1,15 +1,16 @@
 library("truncnorm")
 library("FNN")
 
-GenRandom <- function(m, s, xlo, xhi, ylo, yhi) {
-    dmin <- rtruncnorm(1, 0, mean=m, sd=s)
+genRandom <- function(m, s, xlo, xhi, ylo, yhi) {
+    if (m == 0) { dmin = 0 }
+    else {dmin <- rtruncnorm(1, 0, mean=m, sd=s)}
     x <- runif(1, xlo, xhi)
     y <- runif(1, ylo, yhi)
 
     return(list(x=x, y=y, dmin=dmin))
 }
 
-ValidPoint <- function(rp, points, ngen, dmin) {
+validPoint <- function(rp, points, ngen, dmin) {
     d <- 0
     if (ngen == 0) {
         return(TRUE)
@@ -22,7 +23,7 @@ ValidPoint <- function(rp, points, ngen, dmin) {
         d <- min(knnx.dist(points[1:ngen, ], rp, k=ngen))
     }
 
-    return(d > dmin)
+    return(d >= dmin)
 }
 
 dmin2d <- function(n, m, s, xlo, xhi, ylo, yhi) {
@@ -35,7 +36,7 @@ dmin2d <- function(n, m, s, xlo, xhi, ylo, yhi) {
     ## Output
     ## Returns a n by 2 matrix
 
-    kMaxTries <- 10
+    kMaxTries <- 100
     #matrix to store results
     points <- matrix(data=rep(0, n*2), nrow=n)
     ngen <- 0
@@ -43,9 +44,9 @@ dmin2d <- function(n, m, s, xlo, xhi, ylo, yhi) {
 
     while(ngen < n &
           n.tries < kMaxTries) {
-        r <- GenRandom(m, s, xlo, xhi, ylo, yhi)
+        r <- genRandom(m, s, xlo, xhi, ylo, yhi)
         rp <- matrix(c(r$x, r$y), ncol=2, byrow=T)
-        if (ValidPoint(rp, points, ngen, r$dmin)) {
+        if (validPoint(rp, points, ngen, r$dmin)) {
             n.tries <- 0
             ngen <- ngen + 1
             points[ngen, ] <- rp
@@ -58,9 +59,67 @@ dmin2d <- function(n, m, s, xlo, xhi, ylo, yhi) {
 }
 
 plotDmin2d <- function(points, ...) {
-
     plot(points[, 1], points[, 2], ...)
 }
+
+ri <- function(points) {
+    dists <- knn.dist(points, 1)
+    return(mean(dists) / sd(dists))
+}
+
+riTest <- function(n) {
+    rep.res <- replicate(100, dmin2d(n = n, m = 0, s = 0,
+                                     xlo = 0, xhi = 1000,
+                                     ylo = 0, yhi = 1000),
+                         simplify=FALSE)
+
+    ri.res <- sapply(rep.res, ri)
+    return(sort(ri.res, decreasing=T)[50])
+}
+
+scoreParams <- function(p) {
+    n <- 242
+    real.ri <- 4.753291
+    m <- p[1]
+    s <- p[2]
+
+    rep.res <- replicate(10, dmin2d(n = n, m = m, s = s,
+                                     xlo = 0, xhi = 400,
+                                     ylo = 0, yhi = 400),
+                         simplify=FALSE)
+    ri.res <- sapply(rep.res, ri)
+    avg.ri <- sum(ri.res) / (length(ri.res) - 1)
+
+    return(abs(real.ri - avg.ri))
+}
+
+calcRealRi <- function(fname="spa2.dat") {
+    points <- as.matrix(read.table(fname))
+    return(ri(points))
+}
+
+mcmc <- function() {
+    sigma <- 5
+    T <- 10
+    chain <- matrix(rep(0, T*2), ncol=2)
+    chain[1, ] <- c(10, 5)
+
+    for(t in 2:T) {
+        print(t)
+        ym <- rtruncnorm(1, 0, mean=chain[t-1, 1], sd=sigma)
+        ys <- rtruncnorm(1, 0, mean=chain[t-1, 2], sd=sigma)
+        cat(sprintf("%f %f \n", ym, ys))
+        if (runif(1) > (scoreParams(c(ym, ys)) / scoreParams(chain[t-1, ]))) {
+            chain[t, ] <- c(ym, ys)
+        } else {
+            chain[t, ] <- chain[t-1, ]
+        }
+    }
+
+    return(chain)
+}
+
+
 
 
 
