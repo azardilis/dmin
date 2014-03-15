9aae5ee604011d567b8294fc0782769f777f3e34
Argyris Zardilis <azardilis@gmail.com>
2014-03-15 01:22:46 +0000

    polished some of the plotting function and added functions to investigate different parameter effects

--- dmin.R
+++ dmin.R
@@ -1,9 +1,10 @@
 library("truncnorm")
 library("FNN")
 
+
+## ---- dmin2d ----
 genRandom <- function(m, s, xlo, xhi, ylo, yhi) {
-    if (m == 0) { dmin = 0 }
-    else {dmin <- rtruncnorm(1, 0, mean=m, sd=s)}
+    dmin <- rtruncnorm(1, 0, mean=m, sd=s)
     x <- runif(1, xlo, xhi)
     y <- runif(1, ylo, yhi)
 
@@ -12,9 +13,7 @@ genRandom <- function(m, s, xlo, xhi, ylo, yhi) {
 
 validPoint <- function(rp, points, ngen, dmin) {
     d <- 0
-    if (ngen == 0) {
-        return(TRUE)
-    }
+    if (ngen == 0) return(TRUE)
 
     if (length(points[1:ngen, ]) == 2) {
         data <- matrix(points[1:ngen, ], ncol=2)
@@ -26,7 +25,7 @@ validPoint <- function(rp, points, ngen, dmin) {
     return(d >= dmin)
 }
 
-dmin2d <- function(n, m, s, xlo, xhi, ylo, yhi) {
+dmin2d <- function(n, m, s, xlo, xhi, ylo, yhi, mt=2000) {
     ## Input
     ## n: number of points to simulate
     ## m: mean of Normal distribution
@@ -36,7 +35,9 @@ dmin2d <- function(n, m, s, xlo, xhi, ylo, yhi) {
     ## Output
     ## Returns a n by 2 matrix
 
-    kMaxTries <- 100
+    #special case where m=0
+    if (m == 0) return(cbind(runif(n, xlo, xhi), runif(n, ylo, yhi)))
+    kMaxTries <- mt
     #matrix to store results
     points <- matrix(data=rep(0, n*2), nrow=n)
     ngen <- 0
@@ -58,59 +59,82 @@ dmin2d <- function(n, m, s, xlo, xhi, ylo, yhi) {
     return(points)
 }
 
-plotDmin2d <- function(points, ...) {
-    plot(points[, 1], points[, 2], ...)
+## ---- plotDmin2d ----
+plotDmin2d <- function(points, xlo, xhi, ylo, yhi, ...) {
+    plot(points, pch=16, xlab="x", ylab="y", ...)
+    rect(xlo, ylo, xhi, yhi, lty=2)
 }
 
+## ---- ri ----
 ri <- function(points) {
     dists <- knn.dist(points, 1)
     return(mean(dists) / sd(dists))
 }
 
-riTest <- function(n) {
-    rep.res <- replicate(100, dmin2d(n = n, m = 0, s = 0,
-                                     xlo = 0, xhi = 1000,
-                                     ylo = 0, yhi = 1000),
+## ---- riTest ----
+riTest <- function(n, xhi, yhi) {
+    print(n)
+    rep.res <- replicate(1000, dmin2d(n = n, m = 0, s = 0,
+                                      xlo = 0, xhi = xhi,
+                                      ylo = 0, yhi = yhi),
                          simplify=FALSE)
-
     ri.res <- sapply(rep.res, ri)
-    return(sort(ri.res, decreasing=T)[50])
+    return(ri.res)
+    #return(sort(ri.res, decreasing=T)[50])
 }
 
-scoreParams <- function(p) {
-    n <- 242
-    real.ri <- 4.753291
+## ---- scoreParams ----
+scoreParamsPVal <- function(p, n, real.ri) {
     m <- p[1]
     s <- p[2]
 
-    rep.res <- replicate(10, dmin2d(n = n, m = m, s = s,
-                                     xlo = 0, xhi = 400,
-                                     ylo = 0, yhi = 400),
+    rep.res <- replicate(99, dmin2d(n = n, m = m, s = s,
+                                    xlo = 0, xhi = 400,
+                                    ylo = 0, yhi = 400),
                          simplify=FALSE)
-    ri.res <- sapply(rep.res, ri)
-    avg.ri <- sum(ri.res) / (length(ri.res) - 1)
+    ri.res <- c(real.ri, sapply(rep.res, ri))
+    uis <- sapply(1:length(ri.res), function(x) { abs(ri.res[x] - mean(ri.res[-x])) })
+
+    pval <- match(1, sort(uis, decreasing=T, index.return=T)$ix) / length(uis)
+    return(pval)
+}
+
+scoreParams <- function(p, n, real.ri) {
+    m <- p[1]
+    s <- p[2]
+
+    rep.res <- replicate(99, dmin2d(n = n, m = m, s = s,
+                                    xlo = 0, xhi = 400,
+                                    ylo = 0, yhi = 400),
+                         simplify=FALSE)
+    avg.ri <- mean(sapply(rep.res, ri))
 
     return(abs(real.ri - avg.ri))
 }
 
+## ---- calcRealRi ----
 calcRealRi <- function(fname="spa2.dat") {
     points <- as.matrix(read.table(fname))
     return(ri(points))
 }
 
-mcmc <- function() {
+## ---- mcmc ----
+mcmc <- function(n, real.ri) {
     sigma <- 5
-    T <- 100
+    T <- 200
     chain <- matrix(rep(0, T*2), ncol=2)
-    chain[1, ] <- c(10, 5)
-
+    chain[1, ] <- c(5, 5)
+    prev.score <- scoreParams(chain[1, ], n, real.ri)
+    cat(sprintf("%f %f %f\n", chain[1,1], chain[1,2], prev.score))
     for(t in 2:T) {
         print(t)
         ym <- rtruncnorm(1, 0, mean=chain[t-1, 1], sd=sigma)
         ys <- rtruncnorm(1, 0, mean=chain[t-1, 2], sd=sigma)
-        #cat(sprintf("%f %f \n", ym, ys))
-        if (runif(1) > (scoreParams(c(ym, ys)) / scoreParams(chain[t-1, ]))) {
+        cand.score <- scoreParams(c(ym, ys), n, real.ri)
+        cat(sprintf("%f %f %f\n", ym, ys, cand.score))
+        if (runif(1) > (cand.score / prev.score)) {
             chain[t, ] <- c(ym, ys)
+            prev.score <- cand.score
         } else {
             chain[t, ] <- chain[t-1, ]
         }
@@ -119,22 +143,81 @@ mcmc <- function() {
     return(chain)
 }
 
-
+## ---- failModel ----
 failModel <- function(points) {
     return(any(apply(points, 1, function(x) {any((x == 0) == FALSE)}) == FALSE))
 }
 
+## ---- packDensity ----
 packDensity <- function() {
-    n <- seq(from=50, to=1000, by=50)
-    res <- rep(0, length(n))
-    names(res) <- as.character(n)
-    for (i in 1:length(n)) {
-        print(n[i])
-        points <- dmin2d(n=n[i], m = 20, s = 0, xlo = 0, xhi = 400, ylo = 0, yhi = 400)
-        res[i] <- failModel(points)
+    tries <- seq(from=0, to=1000, by=100)
+    res <- rep(0, length(tries))
+    names(res) <- as.character(tries)
+    for (i in 1:length(tries)) {
+        points <- dmin2d(n=400, m = 20, s = 0, xlo = 0, xhi = 400, ylo = 0,
+                         yhi = 400, mt=tries[i])
+        res[i] <- length(which(apply(points, 1, function(x) { any(x == 0)}) == FALSE))
     }
 
     return(res)
 }
 
+packDensityN <- function() {
+    n <- seq(10, 400, 10)
+    res <- rep(0, length(n))
+    for (i in 1:length(n)) {
+        print(n[i])
+        res[i] <- failModel(dmin2d(n=n[i], 20, 0, 0, 400, 0, 400))
+    }
+    names(res) <- n
 
+    return(tail(names(which(res == 0)), n=1))
+}
+
+
+testArea <- function(area) {
+    side1 <- c(1, 10, 50, 100, 500, 1000, 2500, 5000, 10000, 50000, 100000)
+    side2 <- area / side1
+
+    res <- mapply(riTest, rep(200, length(side1)), side1, side2, SIMPLIFY=FALSE)
+    ri.m <- sapply(res, mean)
+    ri.sd <- sapply(res, sd)
+    ri.50 <- sapply(res, function(x) {sort(x, decreasing=T)[50]})
+
+    return(list(side=side1, ri.m=ri.m, ri.sd=ri.sd, ri.50=ri.50))
+}
+
+plotRIArea <- function(res.ri) {
+    x <- 1:length(res.ri$ri.m)
+    plot(x, res.ri$ri.m, pch=19, xlab="side 1", ylab="mean RI", ylim=c(0.9, 2.0), xaxt='n')
+    arrows(x, res.ri$ri.m-res.ri$ri.sd, x, res.ri$ri.m+res.ri$ri.sd,
+           length=0.05, angle=90, code=3)
+
+    axis(1, at=1:11, labels=as.character(res.ri$side))
+}
+
+
+
+
+dens <- function(d, n , xlo, xhi, ylo, yhi) {
+    ta <- (xhi - xlo) * (yhi - ylo)
+    da <- pi * d^2
+
+    return((1 - (da / ta))^(n-1) * 2*pi*d*n/ta)
+}
+
+exp.d <- function(d, pd) {
+    return(sum(d * pd))
+}
+
+var.d <- function(d, pd, m) {
+    return(sqrt(sum(pd * (d - m)^2)))
+
+}
+
+n <- c(50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000)
+d <- 1:100
+res <- sapply(n, function(x) {dens(d, n=x, 0, 1000, 0, 1000)}, simplify=FALSE)
+exp.rep <- sapply(res, function(x) {exp.d(d, x)})
+i <- 1:length(res)
+var.rep <- sapply(i, function(x) {var.d(d, res[[x]], exp.rep[x])})
